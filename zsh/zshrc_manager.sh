0028592aa2dc0353d493ee504bc3abff200d0a7d
Yi Zhang <yizhang82@outlook.com>
2020-05-27 16:27:45 -0700

    Update zshrc_manager to latest

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,13 +1,23 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 # Run tmux if exists
-#if command -v tmux>/dev/null; then
-#	[ -z $TMUX ] && exec tmux
-#else 
-#	echo "tmux not installed. Run ./deploy to configure dependencies"
-#fi
+if command -v tmux>/dev/null; then
+	[ -z $TMUX ] && exec tmux
+else 
+	echo "tmux not installed. Run ./deploy to configure dependencies"
+fi
 
-#echo "Updating configuration"
-#(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+echo "Checking for updates."
+{cd ~/dotfiles && git fetch -q} &> /dev/null
+
+if [ $({cd ~/dotfiles} &> /dev/null && git rev-list HEAD...origin/master | wc -l) = 0 ]
+then
+	echo "Already up to date."
+else
+	echo "Updates Detected:"
+	{cd ~/dotfiles} &> /dev/null && git log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd
+	echo "Setting up..."
+	{cd ~/dotfiles} &> /dev/null && git pull -q && git submodule update --init --recursive
+fi
 
 source ~/dotfiles/zsh/zshrc.sh
